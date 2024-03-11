a0edfb43e09dc5bf1609f1ae672c84a9bd32a8cf
Rakshit Shetty <rakshitshetty96@gmail.com>
2024-03-11 19:45:10 +0530

    Polyfill for filter

---
+++ filter.js
@@ -0,0 +1,16 @@
+const arr = [10, 12, 15, 19, 30];
+
+//Standard filter function
+const filteredArray = arr.filter(element => element % 2);
+console.log(filteredArray);
+
+Array.prototype.myFilterFunction = function(callback){
+    let result = [];
+    for(let i=0; i<this.length; i++){
+        callback(this[i]) && result.push(this[i]);
+    }
+    return result;
+}
+
+const filteredArrayFromPolyfill = arr.myFilterFunction(element => element % 2);
+console.log(filteredArrayFromPolyfill);
