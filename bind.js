158d146135cc6549449f621c8880165ee5c7eb59
Rakshit Shetty <rakshitshetty96@gmail.com>
2024-03-12 22:09:42 +0530

    Polyfill for bind

---
+++ bind.js
@@ -0,0 +1,24 @@
+function printPersonInfo(age, city){
+    console.log(this.name+" "+age+" "+city);
+}
+
+const person1 = {
+    name: "Rakshit",
+}
+
+const newFunc = printPersonInfo.bind(person1, 28);
+
+newFunc("Bangalore");
+
+Function.prototype.myBind = function(obj={}, ...args1){
+    if(typeof this !== "function"){
+        throw new Error("Not callable");
+    }
+    obj.fn = this;
+    return function(...args2){
+        obj.fn(...args1, ...args2);
+    }
+}
+
+const newBoundedFunc = printPersonInfo.myBind(person1, 28);
+newBoundedFunc("Bangalore");
